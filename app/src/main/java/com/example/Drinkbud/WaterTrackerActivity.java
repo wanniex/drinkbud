924391dbdfba21ca40a88bf0bb5af2f77cfb5473
shimin <shishishishimin@gmail.com>
2020-05-27 04:08:32 +0800

    linked firedatabase

--- app/src/main/java/com/example/Drinkbud/WaterTrackerActivity.java
+++ app/src/main/java/com/example/Drinkbud/WaterTrackerActivity.java
@@ -1,5 +1,6 @@
 package com.example.Drinkbud;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlarmManager;
@@ -12,26 +13,59 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.Calendar;
 
 public class WaterTrackerActivity extends AppCompatActivity {
-    static int num1 = 0; // changed this to static
+    Button waterPlantBtn;
+    EditText waterAmountNum;
+    TextView waterAmountResult;
+
+    FirebaseDatabase rootNode;
+    DatabaseReference reference;
+    int lastDrankRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_water_tracker);
 
-        Button waterPlantBtn = (Button) findViewById(R.id.waterPlantBtn);
+        waterAmountResult = (TextView) findViewById(R.id.waterAmountResult);
+        waterAmountNum = (EditText) findViewById(R.id.waterAmountNum);
+
+        rootNode = FirebaseDatabase.getInstance();
+        reference = rootNode.getReference("waterTracker");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                int lastDrank = Integer.parseInt(dataSnapshot.child("waterDrank").getValue().toString());
+                waterAmountResult.setText("You've drank " + lastDrank + " ml today");
+                lastDrankRef = lastDrank;
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        waterPlantBtn = (Button) findViewById(R.id.waterPlantBtn);
         waterPlantBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                EditText waterAmountNum = (EditText) findViewById(R.id.waterAmountNum);
-                TextView waterAmountResult = (TextView) findViewById(R.id.waterAmountResult);
+
+
+
 
                 int num2 = Integer.parseInt(waterAmountNum.getText().toString());
-                num1 += num2;
-                waterAmountResult.setText("You've drank " + num1 + " ml today");
+                int currentDrank = lastDrankRef + num2;
+                reference.child("waterDrank").setValue(Integer.toString(currentDrank));
+                waterAmountResult.setText("You've drank " + currentDrank + " ml today");
             }
         });
     }
