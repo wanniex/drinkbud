4b2fc7f51c0959920f687e715421876e6e74d869
shimin <shishishishimin@gmail.com>
2020-06-08 14:08:58 +0800

    maps

--- app/src/main/java/com/example/Drinkbud/DrinksStallOptionsActivity.java
+++ app/src/main/java/com/example/Drinkbud/DrinksStallOptionsActivity.java
@@ -1,32 +1,37 @@
 package com.example.Drinkbud;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.core.content.ContextCompat;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
-import com.google.android.gms.location.LocationListener;
+
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 
-public class DrinksStallOptionsActivity extends FragmentActivity implements OnMapReadyCallback,
-        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener,
-        LocationListener {
+public class DrinksStallOptionsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    LocationManager locationManager;
+    LocationListener locationListener;
+    LatLng userLatLong;
 
     private GoogleMap mMap;
-    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,41 +57,68 @@ public class DrinksStallOptionsActivity extends FragmentActivity implements OnMa
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            bulidGoogleApiClient();
-            mMap.setMyLocationEnabled(true);
-        }
-    }
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                // store user latlong
+                userLatLong = new LatLng(location.getLatitude(), location.getLongitude());
+                mMap.clear(); // clear old location marker in google map
+                mMap.addMarker(new MarkerOptions().position(userLatLong).title("Your Location"));
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(userLatLong));
+            }
 
-    private void bulidGoogleApiClient() {
-        client = new GoogleApiClient.Builder(this)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .addApi(LocationServices.API)
-                .build();
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {}
 
-        client.connect();
-    }
+            @Override
+            public void onProviderEnabled(String provider) {}
 
+            @Override
+            public void onProviderDisabled(String provider) {}
+        };
 
-    @Override
-    public void onLocationChanged(Location location) {
-
-    }
-
-
-    @Override
-    public void onConnected(@Nullable Bundle bundle) {
+        // asking for map permission with user/location permission
+        askLocationPermission();
 
     }
 
-    @Override
-    public void onConnectionSuspended(int i) {
+    private void askLocationPermission() {
+        Dexter.withActivity(this).withPermission(Manifest.permission.ACCESS_FINE_LOCATION).withListener(new PermissionListener() {
+            @Override
+            public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                if (ActivityCompat.checkSelfPermission(getBaseContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getBaseContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
 
+                // getting user last location to set the default location maker in the map
+
+                Location lastLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                userLatLong = new LatLng(lastLocation.getLatitude(), lastLocation.getLongitude());
+                mMap.clear(); // clear old location marker in google map
+                mMap.addMarker(new MarkerOptions().position(userLatLong).title("Your Location"));
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(userLatLong));
+            }
+
+            @Override
+            public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+
+            }
+
+            @Override
+            public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                permissionToken.continuePermissionRequest();
+            }
+        }).check();
     }
 
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
-    }
 }
