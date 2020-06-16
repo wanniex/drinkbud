a50d1db912d6b7b718db22b76059006316697cc4
wanniex <65703619+wanniex@users.noreply.github.com>
2020-06-16 21:47:46 +0800

    mapsext

---
+++ app/src/main/java/com/example/Drinkbud/DrinksStallList.java
@@ -0,0 +1,14 @@
+package com.example.Drinkbud;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DrinksStallList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drinks_stall_list);
+    }
+}
--- app/src/main/java/com/example/Drinkbud/DrinksStallOptionsActivity.java
+++ app/src/main/java/com/example/Drinkbud/DrinksStallOptionsActivity.java
@@ -6,12 +6,15 @@ import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -56,6 +59,16 @@ public class DrinksStallOptionsActivity extends FragmentActivity implements OnMa
 
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
         fetchLastLocation();
+
+        Button showMeBtn = (Button) findViewById(R.id.showMeBtn);
+        showMeBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent startIntent = new Intent(getApplicationContext(), DrinksStallList.class);
+                startActivity(startIntent);
+            }
+        });
     }
 
     private void fetchLastLocation() {
